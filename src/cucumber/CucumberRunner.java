b54ae5668bf23bbb4983e9d3a066d50afacd04e1
mandibt <34835964+mandibt@users.noreply.github.com>
2017-12-28 16:42:33 +0100

    CucumberOptions and format deprecation fix in the runnerclass

--- src/cucumber/CucumberRunner.java
+++ src/cucumber/CucumberRunner.java
@@ -2,11 +2,12 @@ package cucumber;
 
 import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(
-		format = {"pretty", "json:target/cucumber.json"},
+@CucumberOptions(
+		plugin = {"pretty", "json:target/cucumber.json"},
 		features = {"src/cucumber/"}
 		) 
 		
